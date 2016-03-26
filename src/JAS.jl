5abd92e23a343b16533313558ccb756c0d108230
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 08:29:00 -0400

    Update JAS.jl

--- src/JAS.jl
+++ src/JAS.jl
@@ -1,20 +1,18 @@
 """
 JAS is a modular bringer of benificence to my own soft work.
 
-
-"""
-module JAS
-
-"""
 Float is an alias for a well defined type of floating point abstraction.
 At its least expansive, Float is a Union{Float64, Float32}.
 Any other type realizing floating point values, that supports
 hash, show, +,-,*,/,sqrt,fma,isless,isequal,==,!=,<,<=,>=,>
 is elegible for inclusion in the set of types that Float covers.
 For example, Float16 and BigFloat could be included.
-""" -> Float
+"""
+module JAS
 
-!isdefined(Float) && typealias Float Union{Float64, Float32, Float16}
+export Float
+
+!isdefined(:Float) && typealias Float Union{Float64, Float32, Float16}
 
 
 
