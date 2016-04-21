9c1b73c6678f952d0605a12a7632a6d88d722b91
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-21 00:53:11 -0400

    Update delegate.jl

--- src/delegate.jl
+++ src/delegate.jl
@@ -17,6 +17,24 @@
 
      macro text from
      https://github.com/JuliaLang/DataStructures.jl/blob/master/src/delegate.jl
+     
+     
+    also this from Toivo for delegation with nary ops
+    (https://groups.google.com/forum/#!msg/julia-dev/MV7lYRgAcB0/-tS50TreaPoJ)
+    
+    julia> type T
+               x
+           end
+
+    julia> import Base.sin, Base.cos
+
+    julia> for f in (:+, :- )    # delegate binary + and - to T.x
+               @eval $f(a::T, b::T) = $f(a.x, b.x)
+           end
+
+    julia> for f in (:sin, :cos) # delegate sin and cos
+               @eval $f(a::T) = $f(a.x)
+           end
 =#
 
 macro delegate(source, targets)
