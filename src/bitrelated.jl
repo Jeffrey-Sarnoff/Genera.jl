2532e0a62def1b76f0ae774cd82052e3c74299a0
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-09 09:46:21 -0400

    Update bitrelated.jl

--- src/bitrelated.jl
+++ src/bitrelated.jl
@@ -2,10 +2,8 @@
 @inline ilog2_core{T::Integer}(x::T) = trailing_zeros(prevpow2(abs(x)))
 @inline ilog2_core{T::AbstractFloat}(x::T) = floor(Int, log2(abs(x)))
 
-ilog2{T:<Union{Integer,AbstractFloat}(x::T) = 
-    ifelse( x>=one(T), 
-            ilog2_core(x),
-            ifelse( x>zero(T),
-                    ilog2_core(x)+one(T),
-                    zero(T)
-          )       )
+ilog2{x::Integer) = 
+    ifelse( x!=zero(T), ilog2_core(x)+(abs(x)!=one(T)), zero(T))
+    
+ilog2(x::AbstractFloat) = 
+    ifelse( x!=zero(T), ilog2_core(x)+(abs(x)<one(T)), zero(T))
