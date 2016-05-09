478eca31b0c64b842273a956c65da5c76a445d23
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-09 11:33:35 -0400

    Update bitrelated.jl

--- src/bitrelated.jl
+++ src/bitrelated.jl
@@ -1,4 +1,12 @@
 #=
+julia> ilog2(15),ilog2(-15),ilog2(16),ilog2(-16),ilog2(17),ilog2(-17)
+(3,3,4,4,4,4)
+
+julia> ilog2(15.),ilog2(-15.),ilog2(16.),ilog2(-16.),ilog2(17.),ilog2(-17.)
+(3,3,4,4,4,4)
+
+julia> ilog2(1/15.),ilog2(-1/15.),ilog2(1/16.),ilog2(-1/16.),ilog2(1/17.),ilog2(-1/17.)
+(3,3,3,3,4,4)
 =#
 
 @inline ilog2_core{T<:Integer}(x::T) = trailing_zeros(prevpow2(abs(x)))
